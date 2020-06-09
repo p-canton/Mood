252a9f77b2a1aeafc9e227569e5e2939f8844edb
Pablo Cantón <pablocg1995@gmail.com>
2020-06-09 12:46:33 +0200

    8
    8

--- src/components/main.js
+++ src/components/main.js
@@ -13,11 +13,11 @@ const Main = () => (
   <>
   <ScrollToTop />
     <Switch>
-      <Route exact path="/Mood/" component={LandingPage}/>
-      <Route path="/Mood/project" component={Project} />
-      <Route path="/Mood/user1" component={User1} />
-      <Route path="/Mood/profile" component={Profile} />
-      <Route path="/Mood/survey" component={Survey} />
+      <Route exact path="/Mood.github.io/" component={LandingPage}/>
+      <Route path="/Mood.github.ioproject" component={Project} />
+      <Route path="/Mood.github.iouser1" component={User1} />
+      <Route path="/Mood.github.ioprofile" component={Profile} />
+      <Route path="/Mood.github.iosurvey" component={Survey} />
     </Switch>
   </>
 )
